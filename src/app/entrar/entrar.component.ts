647ca60e1991351763600292c7522ccc47d0ed3f
unknown <fsergioruiz@hotmail.com>
2021-01-26 15:57:40 -0300

    Implementação do get e post de temas

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -30,7 +30,8 @@ export class EntrarComponent implements OnInit {
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
- 
+      
+      
 
        this.router.navigate(["/inicio"])
     }, erro =>{
